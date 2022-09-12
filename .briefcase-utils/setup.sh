787d7d1b63c0f3c856cd4ef88ee624c661557869
Arthur-Sebastian <artursebastianmiller@gmail.com>
2022-09-12 21:48:21 +0200

    feat: add setup utility
    Implement an automatic setup script for downloading
    submodules and updating bashrc.

---
+++ .briefcase-utils/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ALIAS="git --git-dir=$HOME/.briefcase --work-tree=$HOME"
+
+# basic setup
+echo -en "\nalias briefcase=\"$ALIAS\"" >> $HOME/.bashrc
+$ALIAS config --local status.showUntrackedFiles no
+# submodules
+$ALIAS submodule init
+$ALIAS submodule update
