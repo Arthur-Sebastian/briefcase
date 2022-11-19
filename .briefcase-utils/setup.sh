46fc67cba8dd824430deabf16ace82b8e1e6548b
arthur-sebastian <artursebastianmiller@gmail.com>
2022-11-19 19:03:35 +0100

    feat: add link between nvim and vim configs
    Add a setup step for creating symlinks from vim config
    to nvim configuration directory.

--- .briefcase-utils/setup.sh
+++ .briefcase-utils/setup.sh
@@ -2,9 +2,12 @@
 
 ALIAS="git --git-dir=$HOME/.briefcase --work-tree=$HOME"
 
-# basic setup
-echo -en "\nalias cfg=\"$ALIAS\"" >> $HOME/.bashrc
+# setup repo
 $ALIAS config --local status.showUntrackedFiles no
 # submodules
 $ALIAS submodule init
 $ALIAS submodule update
+# create link between nvim and vim configs
+ln -s $HOME/.vim/pack $HOME/.config/nvim/pack
+ln -s $HOME/.vim/colors $HOME/.config/nvim/colors
+ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
