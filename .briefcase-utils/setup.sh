a5872a817cb110548aa23b92cb0b36027f81e809
arthur-sebastian <artursebastianmiller@gmail.com>
2022-12-18 23:22:15 +0100

    feat(setup): implement iterative tasks
    Add support for iterative npm package installation
    and symlink creation with security checks.

--- .briefcase-utils/setup.sh
+++ .briefcase-utils/setup.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-ALIAS="git --git-dir=$HOME/.briefcase --work-tree=$HOME"
+alias="git --git-dir=$HOME/.briefcase --work-tree=$HOME"
 
-# setup repo
-$ALIAS config --local status.showUntrackedFiles no
-# submodules
-$ALIAS submodule init
-$ALIAS submodule update
-# create link between nvim and vim configs
-ln -s $HOME/.vim/pack $HOME/.config/nvim/pack
-ln -s $HOME/.vim/colors $HOME/.config/nvim/colors
-ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
+printf "Configuring git...\n"
+$alias config --local status.showUntrackedFiles no
+
+printf "Updating sumbodules...\n"
+$alias submodule init
+$alias submodule update
+
+printf "Installing npm packages..."
+nodeDirs=( \
+"$HOME/.config/coc/extensions" \
+)
+for currentDir in "${nodeDirs[@]}"; do
+	(cd $currentDir && npm install)
+done
+
+printf "Creating symbolic links...\n"
+symlinks=( \
+"$HOME/.vim/pack $HOME/.config/nvim/pack" \
+"$HOME/.vim/colors $HOME/.config/nvim/colors" \
+"$HOME/.vimrc $HOME/.config/nvim/init.vim" \
+)
+for currentLink in "${symlinks[@]}"; do
+	linkSource=${currentLink[0]}
+	linkTarget=${currentLink[1]}
+	if [ -f $linkTarget ] || [ -d $linkTarget ]; then
+		continue
+	fi
+	ln -s $linkSource $linkTarget
+done
+
